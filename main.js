296f3c283b94cc45a6e266e3c45e1e12032cdad1
thanhtuan1501 <hoangthanhtuan16dt2@gmail.com>
2021-05-02 23:36:54 +0700

    calo

--- main.js
+++ main.js
@@ -1,33 +1,18 @@
+
 const togglePages = () => {
-    let mainPage = document.querySelector('#main_page').style.display;
-    if(mainPage == 'block'){
+    let mainPage = document.querySelector('#main_page');
+    if(mainPage.style.display == 'block'){
         document.querySelector('#main_page').style.display = 'none';
         document.querySelector('#input_page').style.display = 'block';
     } else {
         document.querySelector('#main_page').style.display = 'block';
         document.querySelector('#input_page').style.display = 'none';
-        
     }
 }
 
 
-var listSubCards = [
-    {   id: 1,
-        imgLink: "./images/sub_img/sub_img1.jpeg",
-        text: "car 1",
-        time: "1",
-    },
-    {   id: 2,
-        imgLink: "./images/sub_img/sub_img2.jpg",
-        text: "bycle 2",
-        time: "2"
-    },
-    {   id: 3,
-        imgLink: "./images/sub_img/sub_img3.jpg",
-        text: "computer 3",
-        time: "3"
-    },
-]
+
+
 const defaut = {
         id: 1,
         imgLink: "./images/sub_img/sub_img1.jpeg",
@@ -68,19 +53,26 @@ const backSubCard = () =>{
 var nowJson = '';
 var saveJson = () =>{
     let time = document.getElementById('time_sub').value;
+    time = parseInt(time);
     let link = document.getElementById('link_sub').value;
     let text = document.getElementById('text_sub').value;
     let id = listSubCards.length +1;
     if(time == ''|| link == '' || text == ''){
         alert('không được bỏ trống')
     } else {
-        var subcard = taoSubCard(link, id , time, text);
-        nowJson = subcard;
-        listSubCards.push(subcard);
+        if (checkRepeat(listSubCards, time) == true){
+            var subcard = taoSubCard(link, id , time, text);
+            nowJson = subcard;
+            listSubCards.push(subcard);
+            listSubCards.sort((a,b) => {
+                return a.time - b.time;
+            })
+        } else {
+            alert('không được đặt trùng sub hình ảnh')
+        }
+
     }
-    listSubCards.sort((a,b) => {
-        return parseFloat(a.time) - parseFloat(b.time);
-    })
+
     document.getElementById('time_sub').value = '';
     document.getElementById('link_sub').value = '';
     document.getElementById('text_sub').value = '';
@@ -88,10 +80,114 @@ var saveJson = () =>{
 
 
 const jsonHientai = () => {
-    if (typeof nowJson.toJson == 'function' ){
-        let json = nowJson.toJson();
-        document.getElementById('json_text').value = json;
-    }
-    console.log(typeof nowJson.toJson )
+    let json = JSON.stringify(nowJson);
+    document.getElementById('json_text').value = json;
+}
+// js cho video youtube
+var tag = document.createElement('script');
+tag.id = 'iframe-demo';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player1;
+var player2;
+function onYouTubeIframeAPIReady() {
+    player1 = new YT.Player('main_video', {
+        height: '390',
+        width: '640',
+        videoId: 'v0hN3UP0PQo',
+        playerVars: {
+          'playsinline': 1
+        },
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+      player2 = new YT.Player('input_video', {
+        height: '390',
+        width: '640',
+        videoId: 'v0hN3UP0PQo',
+        playerVars: {
+          'playsinline': 1
+        },
+        events: {
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
+        }
+      });
+}
+function onPlayerReady(event) {
+
+
 }
 
+function onPlayerStateChange(event) {
+    handleTranscript();
+    
+}
+
+function getCurrentTime () {
+    let time = player2.getCurrentTime()
+    time = Math.round(time*10)/10
+    document.getElementById('time_sub').value = time;
+}
+
+// post subtile cho video 
+function handleTranscript(){
+    if(player1.getPlayerState() == 1){
+        let time1 = player1.getCurrentTime();
+        time1 = Math.floor(time1)
+        putTranScript(time1,sub, time, secondTime)
+        if( checkLock == 1){
+            listSubCards.map(e => {
+                if (e.time == time1){
+                    setSubcard(e);
+                }
+            })
+        }
+        setTimeout(handleTranscript,500)
+    }
+    if(player2.getPlayerState() == 1){
+        let time2 = player2.getCurrentTime();
+        time2 = Math.floor(time2)
+        putTranScript(time2,sub, time, secondTime)  
+        setTimeout(handleTranscript,500)
+    }
+
+}
+var ind = 0;
+function putTranScript (time, subArray, timeArray, secondArray) {
+    let html = document.querySelectorAll('.transcript_video h3');
+    let len = timeArray.length
+    for(let i = 0 ; i < len ; i++){
+        if(secondArray[i] == time){
+            ind = i;
+        }
+    }
+    html.forEach(e => e.innerText = `${timeArray[ind]} -  ${subArray[ind]}`)
+}
+var checkLock = 1;
+const lock = () => {
+    let lockDisplay = document.querySelector('.lock');
+    if(checkLock == 1){
+        lockDisplay.innerText = 'mở khóa'
+        lockDisplay.classList.add('lock-active');
+        checkLock = 0;
+    } else {
+        lockDisplay.classList.remove('lock-active');
+        lockDisplay.innerText = 'đã khóa'
+        checkLock = 1;
+    }
+}
+
+const checkRepeat = (arr, checkValue) => {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        if (arr[i].time == checkValue){
+            return false;
+        }        
+    }
+    return true;
+}
--- subcard.js
+++ subcard.js
@@ -4,9 +4,25 @@ taoSubCard = (imgLink, id, time, text) => {
     subCard.id = id;
     subCard.time = time;
     subCard.text = text;
-    subCard.toJson = function() {
-        return JSON.stringify(this);
-    }
     return subCard;
 }
-// không dùng arrow function trong method của contructor object;
+var listSubCards = [
+    {imgLink: "./images/sub_img/bear.jpg", id: 1, time: 12, text: "bear"},
+    {imgLink: "./images/sub_img/cub.JPG", id: 2, time: 19, text: "cub"},
+    {imgLink: "./images/sub_img/cat.JPG", id: 3, time: 31, text: "cat"},
+    {imgLink: "./images/sub_img/kitten.JPG", id: 4, time: 38, text: "kitten"},
+    {imgLink: "./images/sub_img/chicken.JPG", id: 5, time: 45, text: "chicken"},
+    {imgLink: "./images/sub_img/chick.JPG", id: 6, time: 50, text: "chick"},
+    {imgLink: "./images/sub_img/cow.JPG", id: 7, time: 58, text: "cow"},
+    {imgLink: "./images/sub_img/calf.jpg", id: 8, time: 69, text: "calf"}
+]
+var sub = ["English singsing", "Baby Animals", "bear", "What is the name for a baby bear?", "cub", "It’s a cub.", "Cubs really like honey.", "cat", "What is the name for a baby cat?", "kitten", "It's a kitten.", "chicken", "What is the name for a baby chicken?", "chick", "It’s a chick.", "Chicks are yellow.", "cow", "What is the name for a baby cow?", "calf", "It’s a calf.", "deer", "What is the name for a baby deer?", "fawn", "It’s a fawn.", "dog", "What is the name for a baby dog?", "puppy", "It's a puppy.", "duck", "What is the name for a baby duck?", "duckling", "It’s a duckling.", "frog", "What is the name for a baby frog?", "tadpole", "It’s a tadpole.", "Tadpoles grow into frogs.", "horse", "What is the name for a baby horse?", "foal", "It’s a foal.", "Kangaroo", "What is green for a baby kangaroo?", "joey", "It’s a joey.", "pig", "What is the name for a baby pig?", "piglet", "It’s a piglet.", "Piglets like mud baths.", "rabbit", "What is the name for a baby rabbit?", "bunny", "It’s a bunny.", "sheep", "What is the name for a baby sheep?", "lamb", "It’s a lamb.", "Review"]
+var time = ["00:01", "00:07", "00:12", "00:16", "00:19", "00:21", "00:24", "00:30", "00:34", "00:38", "00:40", "00:44", "00:46", "00:49", "00:51", "00:53", "00:58", "01:05", "01:09", "01:11", "01:16", "01:20", "01:24", "01:26", "01:30", "01:33", "01:37", "01:39", "01:43", "01:47", "01:51", "01:56", "02:01", "02:05", "02:10", "02:12", "02:14", "02:21", "02:24", "02:30", "02:31", "02:37", "02:44", "02:50", "02:53", "02:58", "03:02", "03:05", "03:08", "03:11", "03:16", "03:24", "03:28", "03:29", "03:34", "03:43", "03:47", "03:52", "03:56"]
+var convertSecondTime = (time) => {
+    var timeS = [];
+    for (let i = 0; i < time.length; i++) {
+        timeS[i] = parseInt(time[i].split(":")[0]) * 60 + parseInt(time[i].split(":")[1])
+    }
+    return timeS
+}
+var secondTime = [1, 7, 12, 16, 19, 21, 24, 30, 34, 38, 40, 44, 46, 49, 51, 53, 58, 65, 69, 71, 76, 80, 84, 86, 90, 93, 97, 99, 103, 107, 111, 116, 121, 125, 130, 132, 134, 141, 144, 150, 151, 157, 164, 170, 173, 178, 182, 185, 188, 191, 196, 204, 208, 209, 214, 223, 227, 232, 236]
